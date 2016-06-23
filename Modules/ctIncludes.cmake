a8884e2ffed89518254c4728503176f34c86f74e
Ryan Lindeman <ryanlindeman@gmail.com>
2016-06-22 19:29:06 -0600

    add checking for and enabling C++11 support project wide

--- Modules/ctIncludes.cmake
+++ Modules/ctIncludes.cmake
@@ -1,6 +1,9 @@
 # Include codetemplate variable definitions
 include(ctDefines)
 
+# Include codetemplate C++11 check macros
+include(ctCheckCxx11)
+
 # Include codetemplate macros and functions provided
 include(ctAddDir)
 include(ctAddExe)
